05fbbb096a6cfd17d3008cb7b8efe75df63f38e9
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-11-29 09:22:11 -0500

    appending data in quotes.txt | author.txt | pagination

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import requests
-r = requests.get("https://quotes.toscrape.com/")
-r.encoding = "utf-8"
-# print(r.status_code)
+for i in range(1,11):
+  print("Page :", i)
+  url = f'https://quotes.toscrape.com/page/{i}/'
+  r = requests.get(url)
+  r.encoding = "utf-8"
+  html = r.text
 
-html = r.text
-with open('quotes.txt','w', encoding="utf-8") as f:
-  for line in html.split('\n'):
-    if '<span class="text" itemprop="text">' in line:
-      line = line.replace('<span class="text" itemprop="text">','').replace('</span>','')
-      line = line.strip()
-      line = line.replace('“', '').replace('”', '')
-      f.write(line + "\n")
+  with open('quotes.txt','a', encoding="utf-8") as f:
+    for line in html.split('\n'):
+      if '<span class="text" itemprop="text">' in line:
+        line = line.replace('<span class="text" itemprop="text">','').replace('</span>','')
+        line = line.strip()
+        line = line.replace('“', '').replace('”', '')
+        f.write(line + "\n")
 
-with open('author.txt','w', encoding="utf-8") as f:
-  for line in html.split('\n'):
-    if ' <span>by <small class="author" itemprop="author">' in line:
-      line = line.replace('<span>by <small class="author" itemprop="author">','').replace('</small>','')
-      line = line.strip()
-      f.write(line + "\n")
+  with open('author.txt','a', encoding="utf-8") as f:
+    for line in html.split('\n'):
+      if ' <span>by <small class="author" itemprop="author">' in line:
+        line = line.replace('<span>by <small class="author" itemprop="author">','').replace('</small>','')
+        line = line.strip()
+        f.write(line + "\n")
 
